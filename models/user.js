6e1b63497c2df3ee121ca68ca90098f932b11cb0
Yarden Lawson <yardenlawson@gmail.com>
2021-02-23 21:54:46 +0000

    update

--- models/user.js
+++ models/user.js
@@ -1,15 +1,22 @@
-const mongoose = require('mongoose')
-const bcrypt = require('bcrypt')
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 //* each comment has to fit this schema criteria
-const reviewSchema = new mongoose.Schema({
-  reviewValue: { type: Number, enum: [1, 2, 3, 4, 5], default: 1, required: true },
-  comment: { type: String, required: true },
-  user: { type: mongoose.Schema.ObjectId, ref: 'User', required: true }
-}, {
-  timestamps: true
-})
-
+const reviewSchema = new mongoose.Schema(
+  {
+    reviewValue: {
+      type: Number,
+      enum: [1, 2, 3, 4, 5],
+      default: 1,
+      required: true,
+    },
+    comment: { type: String, required: true },
+    user: { type: mongoose.Schema.ObjectId, ref: 'User', required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
@@ -18,50 +25,38 @@ const userSchema = new mongoose.Schema({
   firstName: { type: String, required: true, maxlength: 50 },
   lastName: { type: String, required: true, maxlength: 50 },
   address: { type: String, required: true, maxlength: 1000 },
-  typeOfUser: { type: String, required: true, maxlength: 50 },
+  typeOfUser: { type: String, renum: ['user', 'client'] },
   profession: { type: String, required: true, maxlength: 500 },
   longitude: { type: Number, required: true, maxlength: 2000 },
   latitude: { type: Number, required: true, maxlength: 2000 },
-  reviews: [reviewSchema]
-})
+  reviews: [reviewSchema],
+});
 
+userSchema.methods.validatePassword = function (password) {
+  return bcrypt.compareSync(password, this.password);
+};
 
-// userSchema
-//   .set('toJSON', {
-//     virtuals: true, 
-//     transform(doc, json) {
-//       delete json.password
-//       return json
-//     }
-//   })
+userSchema.virtual('passwordConfirmation').set(function (passwordConfirmation) {
+  this._passwordConfirmation = passwordConfirmation;
+});
 
-userSchema.methods.validatePassword = function(password) {
-  return bcrypt.compareSync(password, this.password)
-}
+userSchema.pre('validate', function (next) {
+  if (
+    this.isModified('password') &&
+    this._passwordConfirmation !== this.password
+  ) {
+    this.invalidate('passwordConfirmation', 'does not match');
+  }
+  next();
+});
 
-userSchema
-  .virtual('passwordConfirmation')
-  .set(function(passwordConfirmation) {
-    this._passwordConfirmation = passwordConfirmation
-  })
+userSchema.pre('save', function (next) {
+  if (this.isModified('password')) {
+    this.password = bcrypt.hashSync(this.password, bcrypt.genSaltSync(8));
+  }
+  next();
+});
 
-userSchema
-  .pre('validate', function(next) {
-    if (this.isModified('password') && this._passwordConfirmation !== this.password) {
-      this.invalidate('passwordConfirmation', 'does not match')
-    }
-    next()
-  })
+userSchema.plugin(require('mongoose-unique-validator'));
 
-userSchema
-  .pre('save', function(next) {
-    if (this.isModified('password')) {
-      this.password = bcrypt.hashSync(this.password, bcrypt.genSaltSync(8))
-    }
-    next()
-  })
-
-userSchema.plugin(require('mongoose-unique-validator'))
-
-module.exports = mongoose.model('User', userSchema)
-
+module.exports = mongoose.model('User', userSchema);
